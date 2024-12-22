40c2c2f24363e50467917935c910691694e8fa7e
Thomas <Thopic>
2024-12-22 22:09:49 +0100

    CI

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,15 @@ use righor::AminoAcid;
 use righor::EntrySequence;
 use righor::{AlignmentParameters, InferenceParameters};
 
+use std::path::Path;
+
 fn main() -> Result<()> {
     let mut model = Model::load_from_name(
         "human",
         "trb",
         None,
-        &Path(concat!(env!("CARGO_MANIFEST_DIR"), "/righor.data")),
+        &Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/righor.data")),
     )?;
 
-    let ip = InferenceParameters::default();
-    let ap = AlignmentParameters::default();
-
-    let v = genes_matching("TRBV13", &model, false)?;
-    let j = genes_matching("TRBJ1-2", &model, false)?;
-    let seq =
-        EntrySequence::NucleotideCDR3((AminoAcid::from_string("CASSYRGQHKSGYTF").into(), v, j));
-    let res = model.evaluate(seq, &ap, &ip);
-    println!("{:?}", res);
     Ok(())
 }
